4a16e55bfadc96de82a01c7330bab5431e046098
Luis Pedroza <luisk301374@gmail.com>
2019-08-24 23:37:51 -0500

    se agregan los permisos por roles, se agrega la función de cerrar sesión, se oculta la función iniciar sesión después de iniciar sesión

--- BaseDeDatos/Inserts.sql
+++ BaseDeDatos/Inserts.sql
@@ -9,12 +9,41 @@ values('Cédula de Ciudadanía', 'C.C.'), ('Cédula de Extrangería', 'C.E.'), ('Tar
 insert into Personas(PrimerNombre, SegundoNombre, PrimerApellido, SegundoApellido, TiposDeDocumentoId, NumeroDocumento, GeneroId)
 values ('Luis', 'Carlos', 'Pedroza', 'Pineda', 1, '1079935561', 1);
 
+insert into Personas(PrimerNombre, SegundoNombre, PrimerApellido, SegundoApellido, TiposDeDocumentoId, NumeroDocumento, GeneroId)
+values ('Olmedo', 'Rafael', 'Pedroza', 'Barrios', 1, '7594393', 1);
+
 insert into Tallas (nombre) values (14), (12), (10), (8), (6);
 
 insert into Permisos (Codigo)
 values ('CREAR_USUARIO'), ('ELIMINAR_USUARIO'), ('EDITAR_USUARIO'), ('MATRICULAR_ALUMNO'), ('EDITAR_ALUMNO'), ('ELIMINAR_ALUMNO');
+insert into Permisos (Codigo) values('CREAR_ADMINISTRADOR');
+insert into Permisos (Codigo) values('VER_USUARIOS'), ('VER_ALMUNOS');
 
 insert into EstadosAlumnos (id,Nombre) values (1,'Matriculado'), (2,'Inscrito'), (3,'Suspendido');
 
 insert into Usuarios (Correo, Celular, NombreUsuario, Clave, PersonaId, RolId)
 values ('luisk301374@gmail.com', '3004255615','luisk','lmuairsy',2, 1);
+
+insert into Usuarios (Correo, Celular, NombreUsuario, Clave, PersonaId, RolId)
+values ('olrape67@gmail.com', '3023797554','olrape','o7594393',3, 2);
+
+-- Super Administrador
+insert into PermisosPorRoles (idRol, idPermisos) values (1,1);
+insert into PermisosPorRoles (idRol, idPermisos) values (1,2);
+insert into PermisosPorRoles (idRol, idPermisos) values (1,3);
+insert into PermisosPorRoles (idRol, idPermisos) values (1,4);
+insert into PermisosPorRoles (idRol, idPermisos) values (1,5);
+insert into PermisosPorRoles (idRol, idPermisos) values (1,6);
+insert into PermisosPorRoles (idRol, idPermisos) values (1,7);
+
+-- Administrador
+insert into PermisosPorRoles (idRol, idPermisos) values (2,1);
+insert into PermisosPorRoles (idRol, idPermisos) values (2,2);
+insert into PermisosPorRoles (idRol, idPermisos) values (2,3);
+insert into PermisosPorRoles (idRol, idPermisos) values (2,4);
+insert into PermisosPorRoles (idRol, idPermisos) values (2,5);
+insert into PermisosPorRoles (idRol, idPermisos) values (2,6);
+
+-- Entrenador
+insert into PermisosPorRoles (idRol, idPermisos) values (3,4);
+insert into PermisosPorRoles (idRol, idPermisos) values (3,5);
---
+++ src/java/co/escuelatp/controladores/LogoutControlador.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.escuelatp.controladores;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author user
+ */
+@WebServlet(name = "LogoutControlador", urlPatterns = {"/LogoutControlador"})
+public class LogoutControlador extends HttpServlet {
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession sesion = request.getSession();
+        
+        sesion.removeAttribute("USUARIO");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        dispatcher.forward(request, response);
+    }
+
+   
+}
